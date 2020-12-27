7916f45e3a2d5436052c36d296671d88a9313c89
KwongTN <tungnan5636@gmail.com>
2020-12-27 15:30:29 +0800

    [Angular] Added about page descriptions
    For other group members

--- public/src/app/about/team/about-team.component.ts
+++ public/src/app/about/team/about-team.component.ts
@@ -25,12 +25,23 @@ export class AboutTeamComponent implements OnInit {
       role: 'Lead Developer',
       profilePict:
         'https://media-exp1.licdn.com/dms/image/C5103AQHH8z3LUfFBLw/profile-displayphoto-shrink_200_200/0/1559217292341?e=1614816000&v=beta&t=trFMxoKyIu20Re9J3IlB9FZm-3yFspzNgmxwcH_98RU',
-      description: `A passionate individual that is constantly exploring and being amazed by new technologies. Always open to learn them to create a better world, I won't mind if credit is not given to me, as long as the end goal is achieved.
-      I devote my free time on understanding and joining talks, forums, meetups and exhibitions on improving my skillsets, discuss on ways to improve connectivity in Klang Valley with regards to the Sustainable Development Goals, and to advocate for Open Data.
-      Also a paranoid for good UI/UX, future-proof implementations and massively scalable systems.`,
+      description: `A passionate individual that is constantly exploring and being amazed by new technologies. Always open to learn them to create a better world, I won't mind if credit is not given to me, as long as the end goal is achieved. \nI devote my free time on understanding and joining talks, forums, meetups and exhibitions on improving my skillsets, discuss on ways to improve connectivity in Klang Valley with regards to the Sustainable Development Goals, and to advocate for Open Data.\nAlso a paranoid for good UI/UX, future-proof implementations and massively scalable systems.`,
       links: [
         { name: 'LinkedIn', link: 'https://www.linkedin.com/in/kwongtn/' },
         { name: 'GitHub', link: 'https://github.com/kwongtn' },
+      ],
+    },
+    {
+      name: 'Shamhinidevi Jayabalan',
+      role: 'Backend API Developer',
+      profilePict: 'assets/sham.jpg',
+      description: `I'm Shamhinidevi an undergraduate student. I am currently pursuing my third year Bachelor of Degree in Computer Science specializes in Artificial Intelligence. In 2018, I graduated from Politeknik Balik Pulau (PBU). \nI am a person who is positive about every aspect of life. I love to explore what's around me, everything interests me very much. Life is all about growth. I am still learning, and sometimes it is wonderful and other times it is messy, but I keep moving. `,
+      links: [
+        {
+          name: 'LinkedIn',
+          link: 'https://www.linkedin.com/in/shamhinidevi-jayabalan-ba5a77159',
+        },
+        { name: 'GitHub', link: 'https://github.com/Sham0106' },
       ],
     },
   ];
