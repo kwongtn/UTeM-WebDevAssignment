4d8d09655a3b2ad4fc3d236c67ccb589b9a4700b
AinSofiya <65455966+AinSofiya@users.noreply.github.com>
2021-01-12 13:16:30 +0800

    Update database.php

---
+++ API/database.php
@@ -0,0 +1,25 @@
+<?php
+class Database{
+  
+    
+    private $host = "48mh42.ddns.net:51703";
+    private $db_name = "webdb";
+    private $username = "root";
+    private $password = "tnwebdev123";
+    public $conn;
+  
+    // get the database connection
+    public function getConnection(){
+  
+        try{
+            $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name,$this->db_username,$this->db_password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        }
+        catch(PDOException $e){
+            echo "Connection error ".$e->getMessage(); 
+            exit;
+        }
+    }
+}
+?>
