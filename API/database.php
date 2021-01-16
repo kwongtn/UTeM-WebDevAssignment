1b1581f60328b7425c30b7f05caa39de52b0337f
AinSofiya <B031810081@student.utem.edu.my>
2021-01-17 03:07:46 +0800

    Update database.php

--- API/database.php
+++ API/database.php
@@ -2,25 +2,24 @@
 class Database{
   
     
-    private $host = "48mh42.ddns.net:51703";
+    private $db_host = "48mh42.ddns.net:51703";
     private $db_name = "webdb";
-    private $username = "root";
-    private $password = "tnwebdev123";
+    private $db_username = "root";
+    private $db_password = "tnwebdev123";
     public $conn;
   
     // get the database connection
     public function getConnection(){
-  
+        
         try{
-            $conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
+            $conn = new PDO("mysql:host=" . $this->db_host . ";dbname=" . $this->db_name, $this->db_username, $this->db_password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $conn;
+            
         }
         catch(PDOException $e){
             echo "Connection error ".$e->getMessage(); 
             exit;
         }
+        return $this->conn;
     }
 }
-?>
