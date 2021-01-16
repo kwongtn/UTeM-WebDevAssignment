6d3c17ff497bf25b7bd34bf772051d72ad189ffa
KwongTN <tungnan5636@gmail.com>
2021-01-16 19:32:24 +0800

    [Ang] Specific content will be displayed with respect to login status

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { SessionService } from 'src/services/session.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'public';
+  loginStatus: boolean = false;
+
+  constructor(private sessionService: SessionService) {
+    this.sessionService.loginStatus.subscribe((res: boolean) => {
+      this.loginStatus = res;
+    });
+  }
 }
