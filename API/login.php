35bebde12a0922fa56eadf50a6410debf85f1e90
Sham0106 <63240613+Sham0106@users.noreply.github.com>
2021-01-17 00:52:49 +0800

    Update login.php

--- API/login.php
+++ API/login.php
@@ -1,13 +1,13 @@
 <?php
-include_once("database.php");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$db_connection = new Database();
-$conn = $db_connection->dbConnection();
+include_once( 'database.php' );
+$database = new Database();
+$db = $database->getConnection();
 
 //class User
 //{
