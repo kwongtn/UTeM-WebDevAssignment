ff6ebb8fc5dd2865f73add80ff90af8778b0687f
AinSofiya <b031810081@student.utem.edu.my>
2021-01-16 12:30:19 +0800

    area list

---
+++ API/areaClass.php
@@ -0,0 +1,41 @@
+<?php
+class Area{
+  
+    // database connection and table name
+    private $conn;
+    private $table_name = "AREA";
+  
+    // object properties
+    public $areaID;
+    public $areaName;
+    
+  
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    // read products
+    function read(){
+    
+        // select all query
+        $query = "SELECT
+                    a.name as areaName
+                FROM
+                    " . $this->table_name . " a
+                    LEFT JOIN
+                        AREA al
+                            ON a.areaID = al.id
+                ORDER BY
+                    a.created DESC";
+    
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+    
+        // execute query
+        $stmt->execute();
+    
+        return $stmt;
+    }
+}
+?>
