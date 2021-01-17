67c337d5af8aa43aa74a0478659ea5ea75c297c4
AinSofiya <B031810081@student.utem.edu.my>
2021-01-17 23:13:45 +0800

    Update database.php

--- API/database.php
+++ API/database.php
@@ -10,16 +10,16 @@ class Database{
   
     // get the database connection
     public function getConnection(){
-        
+  
         try{
-            $conn = new PDO("mysql:host=" . $this->db_host . ";dbname=" . $this->db_name, $this->db_username, $this->db_password);
+            $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name,$this->db_username,$this->db_password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
+            return $conn;
         }
         catch(PDOException $e){
             echo "Connection error ".$e->getMessage(); 
             exit;
         }
-        return $this->conn;
     }
 }
+?>
