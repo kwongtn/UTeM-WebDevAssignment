12c074812c243e2f56a1cf3333a79fc8bb33ea5e
AinSofiya <B031810081@student.utem.edu.my>
2021-01-17 22:02:35 +0800

    Update area-list.php

--- API/area-list.php
+++ API/area-list.php
@@ -2,65 +2,41 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-  
+
 // database connection will be here
 // include database and object files
 //include_once './database.php';
 include_once('database.php');
 //include_once './areaClass.php';
-include_once('areaClass.php');
-  
+//include_once('areaClass.php');
+
 // instantiate database and area object
 $database = new Database();
 $db = $database->getConnection();
-  
-// initialize object
-$areaClass = new Area($db);
-  
-// read area will be here
-// query area
-$stmt = $areaClass->read();
-$num = $stmt->rowCount();
-  
-// check if more than 0 record found
-if($num>0){
-  
-    // area array
-    $AREA_arr = array();
-    $AREA_arr["AREA"]=array();
-  
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        // extract row
-        // this will make $row['name'] to
-        // just $name only
-        extract($row);
-  
-        $AREA_details=array(
-            "areaID" => $areaID,
-            "areaName" => $areaName  
-        );
-  
-        array_push($AREA_arr["AREA"], $AREA_details);
-    }
-  
+$num=array();
+
+if (count($num) > 0) {
+       
+    // select area array from array table
+    $query = "SELECT * FROM webdb.AREA";  
+    $result = $db->query($query);
+
     // set response code - 200 OK
     http_response_code(200);
-  
+
     // show area data in json format
-    echo json_encode($AREA_arr);
+    echo json_encode($result);
 }
-  
+
 // no area found will be here
-else{
-  
+else {
+
     // set response code - 404 Not found
     http_response_code(404);
-  
+
     // tell the user no area found
     echo json_encode(
         array("message" => "No area found.")
     );
 }
+?>
